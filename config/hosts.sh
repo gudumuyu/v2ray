3225051a2e9c06620d8dfc8db0a67c002ffc77ad
gudumuyu <32303402+gudumuyu@users.noreply.github.com>
2020-10-03 17:40:22 +0800

    Update hosts.sh

--- config/hosts.sh
+++ config/hosts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #  生成要屏蔽的域名... MVPS HOSTS​​​​​​​​​....
-curl  "http://winhelp2002.mvps.org/hosts.txt" -o hosts.txt
+#curl  "http://winhelp2002.mvps.org/hosts.txt" -o hosts.txt
 sed -i '/^#/d; /localhost/d; /^$/d; s/0.0.0.0 //g; s/ //' hosts.txt
 sed -i 's/#\(.*\)//g' hosts.txt
 sed -i '/^\s*$/d' hosts.txt
